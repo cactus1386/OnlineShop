ef234c21622adbd63a9f48fef0d51bdbf19645f3
ARS6666 <arsprogramming123@gmail.com>
2024-07-01 16:30:34 -0700

    user info completed

--- src/components/RecentlyPurchasedPrdcts.js
+++ src/components/RecentlyPurchasedPrdcts.js
@@ -1,75 +1,63 @@
 import React from 'react'
+import { useState } from 'react';
 import './headers/InvoiceHeader.js';
-import Iheader from './headers/InvoiceHeader.js';
 import '../font/font.css'
 
-import BasicPopover from './Featuress/Popover.js';
-// ال آی قراره کارد درست کنی وقتی دکمه ی مشخصات رو زدی یه کارد باز بشه و مشخصات محصول رو نشون بدهو اینکخه از قسمت سیمبل ها اون پایین هم 
-// تگ کردی قراره عکست اون دستور باشه
 function RPP() {
-    return(
-		<div class="card card-custom gutter-b pr-7 mt-5 ml-10 mr-10 text-right fontv" dir='rtl'>
-											<div class="card-body p-0">
-        <div class="row justify-content-center py-8 px-8 py-md-10 px-md-0">
-			
-													<div class="col-md-10"><Iheader></Iheader>
-														<div class="table-responsive">
-														
-															<table class="table">
-																<thead>
-																	<tr>
-																		<th class="pl-0 font-weight-bold text-muted text-uppercase">سفارشات</th>
-																		<th class="text-right font-weight-bold text-muted text-uppercase">تعداد</th>
-																		<th class="text-right font-weight-bold text-muted text-uppercase">قیمت هرکدام</th>
-																		<th class="text-right pr-0 font-weight-bold text-muted text-uppercase">قیمت کل</th>
-																	</tr>
-																</thead>
-																<tbody>
-																	<tr class="font-weight-boldest">
-																		<td class="border-0 pl-0 pt-7 d-flex align-items-center">
-																	
-																		<div class="symbol symbol-40 flex-shrink-0 mr-4 bg-light">
-																			<div class="symbol-label" style={{backgroundImage: "url(" + "assets/media/products/11.png" + ")"}}></div>
-																		</div>
-																	
-																		پیراهن</td>
-																		<td class="text-right pt-7 align-middle">2</td>
-																		<td class="text-right pt-7 align-middle">$90.00</td>
-																		<td class="text-primary pr-0 pt-7 text-right align-middle">$180.00</td>
-																		<BasicPopover/>
-																	</tr>
-																	<tr class="font-weight-boldest border-bottom-0">
-																		<td class="border-top-0 pl-0 py-4 d-flex align-items-center">
-																		
-																		<div class="symbol symbol-40 flex-shrink-0 mr-4 bg-light">
-																			<div class="symbol-label" style={{backgroundImage: "url(" + 'assets/media/products/2.png' + ")"}}></div>
-																		</div>
-																		هدفون</td>
-																		<td class="border-top-0 text-right py-4 align-middle">1</td>
-																		<td class="border-top-0 text-right py-4 align-middle">$449.00</td>
-																		<td class="text-primary border-top-0 pr-0 py-4 text-right align-middle">$449.00</td>
-													                      <BasicPopover/>
-																	</tr>
-																	<tr class="font-weight-boldest border-bottom-0">
-																		<td class="border-top-0 pl-0 py-4 d-flex align-items-center">
-																		
-																		<div class="symbol symbol-40 flex-shrink-0 mr-4 bg-light">
-																			<div class="symbol-label" style={{backgroundImage: "url(" + 'assets/media/products/1.png' + ")"}}></div>
-																		</div>
-																	
-																		بسته ی  کاغذ</td>
-																		<td class="border-top-0 text-right py-4 align-middle">1</td>
-																		<td class="border-top-0 text-right py-4 align-middle">$160.00</td>
-																		<td class="text-primary border-top-0 pr-0 py-4 text-right align-middle">$160.00</td>
-																		<BasicPopover/>
-																	</tr>
-																</tbody>
-															</table>
-														</div>
-													</div>
-												</div>
-												</div>
-														</div>
+	const [state,setState]=useState([
+        {Id:'1',Name : "نیکه ترو بالانث", Price:'455,000' ,Amount : '69',Date: "05/28/2020", PaymentStatus:'پرداخت شده',PriceSum :'959,934,000'},
+      ])
+    return(<>
+		<div class='d-flex justify-content-center pt-5 fontv'>
+					<div class="card card-custom col-md-9 " dir='rtl'>
+						<div class="card-header border-0 pt-2">
+							<h4 class="card-title align-items-center flex-column">
+								<span class="card-label font-weight-bolder text-dark"> لیست سفارشات قبلی</span>
+							</h4>
+						</div>
+						<div class="card-body py-0">
+							<div class="table-responsive">
+								<table class="table table-sm table-bordered table-hover table-vertical-center" id="kt_advance_table_widget_4">
+									<thead>
+										<tr class="text-left">
+											<th class="pl-0" style={{width: 30+'px'}}>
+											</th>
+											<th class="pl-0" style={{'min-width': 120+"px"}}>نام</th>
+											<th style={{'min-width':110+"px"}} >تعداد</th>
+											<th style={{'min-width':110+"px"}} > قیمت کل</th>
+											
+										</tr>
+									</thead>
+									<tbody>
+									{state.map(c=>
+										<tr>
+											<td class="pl-0 py-6">
+												<span class="text-dark-75 font-weight-bolder d-block font-size-lg">{c.Id}</span>
+											</td>
+											<td class="pl-0">
+												<a href="product" class="text-dark-75 font-weight-bolder text-hover-primary font-size-lg">{c.Name}</a>
+											</td>
+											<td>
+												<span class="text-dark-75 font-weight-bolder d-block font-size-lg">{c.Amount}</span>
+											</td>
+											<td>
+												<span class="text-dark-75 font-weight-bolder d-block font-size-lg">{c.PriceSum}</span>
+												<span class="text-muted font-weight-bold">تومان</span>
+											</td>
+										
+
+											
+										</tr>)	}
+									
+									
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+                
+                </>
 
     )
     
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from 'react'
 import Cart from './Cart'
+import RPP from './RecentlyPurchasedPrdcts';
+
 const UserInfo  = () => {
   const [Infos, setInfos] = useState([]);
   const [isLoading, setLoading] = useState(true)
@@ -27,41 +29,46 @@ const UserInfo  = () => {
   };
 
     return (<>
+            <div class='col-md-12'>
                 <div class="d-flex justify-content-center col-md-12">
-                <div className="col-md-8 bg-light border rounded border fontv" dir= "rtl">
-                    <div class="card-body pt-4 ">
-                        <div class=" justify-content-end">
-                            <div class="d-flex align-items-end">
-                                <div class="d-flex align-items-left">
-                                    <div class="flex-shrink-0 mr-4 mt-lg-0 mt-3">
-                                        <div class="symbol symbol-circle symbol-lg-75">
-                                            <img src={Infos.image} alt="image"></img>
+                    <div className="col-md-9 bg-light border rounded border fontv" dir= "rtl">
+                        <div class="card-body pt-4 ">
+                            <div class=" justify-content-end">
+                                <div class="d-flex align-items-end">
+                                    <div class="d-flex align-items-left">
+                                        <div class="flex-shrink-0 mr-4 mt-lg-0 mt-3">
+                                            <div class="symbol symbol-circle symbol-lg-75">
+                                                <img src={Infos.image} alt="image"></img>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                            <a href="#" class="text-dark font-weight-bold text-hover-primary font-size-h4 mb-0">{Infos.name}</a>
                                         </div>
                                     </div>
-                                    <div class="d-flex flex-column">
-                                        <a href="#" class="text-dark font-weight-bold text-hover-primary font-size-h4 mb-0">{Infos.name}</a>
+                                </div>
+                                <div class="mb-7 ">
+                                    <div class="d-flex justify-content-between align-items-center pt-2">
+                                        <span class="text-dark-75 font-weight-bolder mr-2">شماره همراه:</span>
+                                        <a href="#" class="text-muted text-hover-primary">{Infos.phone}</a>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="mb-7 ">
-                                <div class="d-flex justify-content-between align-items-center pt-2">
-                                    <span class="text-dark-75 font-weight-bolder mr-2">شماره همراه:</span>
-                                    <a href="#" class="text-muted text-hover-primary">{Infos.phone}</a>
-                                </div>
 
-                                <div class="d-flex justify-content-between align-items-center pt-3">
-                                    <span class="text-dark-75 font-weight-bolder mr-2">ایمیل:</span>
-                                    <a href="#" class="text-muted text-hover-primary">{Infos.phone}</a>
-                                </div>
-                                <div class="d-flex justify-content-between align-items-center pt-3 pb-3">
-                                    <span class="text-dark-75 font-weight-bolder mr-2">آدرس:</span>
-                                    <span class="text-muted font-weight-bold">{Infos.description}</span>
+                                    <div class="d-flex justify-content-between align-items-center pt-3">
+                                        <span class="text-dark-75 font-weight-bolder mr-2">ایمیل:</span>
+                                        <a href="#" class="text-muted text-hover-primary">{Infos.phone}</a>
+                                    </div>
+                                    <div class="d-flex justify-content-between align-items-center pt-3 pb-3">
+                                        <span class="text-dark-75 font-weight-bolder mr-2">آدرس:</span>
+                                        <span class="text-muted font-weight-bold">{Infos.description}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                </div>
+            <Cart />
+            <RPP/>
+        </div>
+                
                 
      {/* .. */}
          </>
