3eb296eddf2efc91ca5a941f33349d4f398cd3fa
ARS6666 <arsprogramming123@gmail.com>
2024-07-02 00:15:42 -0700

    some changes in routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ProductList from './components/ProductList';
-import Cart from './components/Cart';
+import Cart from './components/ProfileCart';
 import Login from './components/Login';
 import RPP from './components/RecentlyPurchasedPrdcts';
 import ShoppingNavbar from './components/Nav';
 import SignIn from './components/SignIn';
+import Profile from './components/UserInfo';
+import HCart from './components/HomeCart';
 
 function App() {
   return (
@@ -18,8 +20,9 @@ function App() {
           <Route path='/product' element={<ProductList />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<SignIn />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path='/cart' element={<HCart />} />
           <Route path='/rpp' element={<RPP />} />
+          <Route path='/user' element={<Profile />} />
         </Routes>
       </div>
     </Router>
