83e9ef4be7a55b491161018d5fe0be6e857486d5
cactus1386 <cactusinjast@gmail.com>
2024-07-02 16:12:05 -0700

    update dark mode

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav, Form, FormControl, Button, NavDropdown, Container } from 'react-bootstrap';
+import { Modal, FormControl, FormGroup, FormLabel, Button, ModalFooter, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/Nav.css';
 import '../font/font.css';
-import CWin from './headers/CollapseWin';
 
 const ShoppingNavbar = () => {
 	const [expanded, setExpanded] = useState(false);
-	const [isDarkMode, setIsDarkMode] = useState(false);
+	const [isDarkMode, setIsDarkMode] = useState(() => {
+		const savedTheme = localStorage.getItem('darkMode');
+		return savedTheme === 'true';
+	});
 
 	useEffect(() => {
 		if (isDarkMode) {
@@ -15,14 +17,27 @@ const ShoppingNavbar = () => {
 		} else {
 			document.body.classList.remove('dark-mode');
 		}
+		localStorage.setItem('darkMode', isDarkMode);
 	}, [isDarkMode]);
 
 	const handleToggle = () => setExpanded(!expanded);
 	const closeNav = () => setExpanded(false);
 	const handleModeSwitch = () => setIsDarkMode(!isDarkMode);
 
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const inputStyle = { height: '50px' };
+	const buttonn = {
+		height: '37px',
+		width: '100%',
+		marginBottom: '15px',
+		borderRadius: '1px'
+	};
+
 	return (
-		<nav className="shopping-navbar fontv d-flex" dir='rtl'>
+		<nav className="shopping-navbar fontv d-flex" dir="rtl">
 			<div className="navbar-container pb-5">
 				<div className="navbar-logo">
 					<img
@@ -34,9 +49,8 @@ const ShoppingNavbar = () => {
 				</div>
 				<div className="navbar-links">
 					<a href="/">خانه</a>
-
 					<div className="dropdown">
-						<a className="btnn" href='product'>محصولات</a>
+						<a className="btnn" href="product">محصولات</a>
 						<div className="dropdown-content">
 							<a href="#electronics">الکترونیک</a>
 							<a href="#clothing">لباس</a>
@@ -50,7 +64,12 @@ const ShoppingNavbar = () => {
 					<button>جستجو</button>
 					<input type="text" placeholder="دنبال چه می گردید؟..." />
 				</div>
-				<CWin />
+				<div className="navbar-icons">
+					<a href="#account" className="icon-link" onClick={handleShow}>
+						<i className="icon">👤</i>حساب کاربری
+					</a>
+					<a href="cart" className="icon-link"><i className="icon">🛒</i> سبد خرید (0)</a>
+				</div>
 				<div className="mode-switch-container">
 					<span>{isDarkMode ? '🌙' : '☀️'}</span>
 					<input
