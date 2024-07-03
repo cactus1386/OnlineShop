3af8304c33b38540e8a57567b8d8cf0b87bc08ab
cactus1386 <cactusinjast@gmail.com>
2024-07-02 19:01:54 -0700

    about

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import SignIn from './components/SignIn';
 import Profile from './components/UserInfo';
 import HCart from './components/HCart';
 import Home from './components/Home';
-
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
@@ -19,13 +19,14 @@ function App() {
       <ShoppingNavbar />
       <div className="main-content">
         <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<AboutUs />} />
           <Route path='/product' element={<ProductList />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<SignIn />} />
           <Route path='/cart' element={<HCart />} />
           <Route path='/rpp' element={<RPP />} />
           <Route path='/user' element={<Profile />} />
-          <Route path='home' element={<Home />} />
         </Routes>
       </div>
     </Router>
