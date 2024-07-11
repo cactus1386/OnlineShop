066cc4830b33b9a71b835bd650d610be2999c3ff
ARS6666 <arsprogramming123@gmail.com>
2024-07-11 10:37:45 -0700

    slider has bug (: its bc of database

--- src/components/FullScreenAlbum.js
+++ src/components/FullScreenAlbum.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import { Carousel } from 'react-bootstrap';
-import img1 from '../assets/media/products/1.png'
-import img2 from '../assets/media/products/2.png'
-import img3 from '../assets/media/products/4.png'
-import img4 from '../assets/media/products/23.PNG'
-import '../assets/css/General.css';
+// import React from "react";
+// import { useState } from "react";
+// import 'bootstrap/dist/css/bootstrap.css';
+// import { Carousel } from 'react-bootstrap';
+// import img1 from '../assets/media/products/1.png'
+// import img2 from '../assets/media/products/2.png'
+// import img3 from '../assets/media/products/4.png'
+// import img4 from '../assets/media/products/23.PNG'
+// import '../assets/css/General.css';
 
 
-function ImageSlider () {
-  const [state,setState]=useState([
-    {Image:img1},
-    {Image:img2},
-    {Image:img3},
-  ])
-  return (
-    <div class=" col-md-12 gallery">
-      <img class="  gallery bgimg" src={img4}/>
-      <div class="d-flex justify-content-center pt-5">
-        <div class="container-xl col-md-5 ">
-          <Carousel>
-          {state.map(c=><Carousel.Item>
-              <img className="d-block w-100" src={c.Image} alt="Third slide" />
-            </Carousel.Item>
-          )}
-          </Carousel>
-          </div>
-        </div>
-      </div>
-  );
-};
+// function ImageSlider () {
+//   const [state,setState]=useState([
+//     {Image:img1},
+//     {Image:img2},
+//     {Image:img3},
+//   ])
+//   return (
+//     <div class=" col-md-12 gallery">
+//       <img class="  gallery bgimg" src={img4}/>
+//       <div class="d-flex justify-content-center pt-5">
+//         <div class="container-xl col-md-5 ">
+//           <Carousel>
+//           {state.map(c=><Carousel.Item>
+//               <img className="d-block w-100" src={c.Image} alt="Third slide" />
+//             </Carousel.Item>
+//           )}
+//           </Carousel>
+//           </div>
+//         </div>
+//       </div>
+//   );
+// };
 
-export default ImageSlider;
+// export default ImageSlider;
