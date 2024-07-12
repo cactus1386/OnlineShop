08658d47ee6fba72d95150bf01fff2039230bc7f
Elay <li0elay0li@gmail.com>
2024-07-12 01:05:31 -0700

    comment fixed

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -42,10 +42,22 @@ const Comments = ({ productId }) => {
     };
 
     fetch("http://94.183.74.154:1234/comments/api/v1/post", requestOptions)
-      .then(response => response.text())
-      .then(result => show())
-      .catch(error => console.log('error', error));
-  };
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(error => { throw new Error(error.message); });
+        }
+        return response.json();
+      })
+      .then(result => {
+        setComment('');
+        setName('');
+        show();
+      })
+      .catch(error => {
+        console.log('Error posting comment:', error);
+        setLoading(false);
+      });
+  }
 
   return (
     <div className="d-flex justify-content-center col-md-12">
@@ -54,39 +66,51 @@ const Comments = ({ productId }) => {
           <div className="loadingPage">
             <div className="spinner spinner-dark mr-15 text-center loadingCenter"></div>
           </div>
-        </div>
-      )}</div>
-      <div class="d-flex align-items-center">
-      <div class=" col-md-10 rounded p-5 mb-9 d-flex align-items-center">
-        <input type='text' id="1" onChange={e => setComment(e.target.value)} class="form-control form-control-solid form-control-lg col-md-2" ></input>
-        <button onClick={sendToServer} class=" col-md-2 btn btn-primary font-weight-bold">ثبت</button>
-      </div>
-      </div>
-      </div> : 
-      <div>
-      <div class='contain'>{Comments.map(c =>
-        <div>
-          <div class="mt-2 rounded p-5 border text-dark-50 font-weight-bold font-size-lg text-left max-w-1500px fontv">
-              <div class="text-muted ">{c.name}:</div>
-              <div class='h4'>{c.content}</div>
-          </div>
-        </div>
-      )}</div>
-        <div class="pt-4 ">
-          <div class="pt-2 border-top border-dark">
-            <div class='card '>
-              <div class="d-flex justify-content-start ">
-                <div class=" col-md-10 rounded p-3 d-flex align-items-center">
-                  <input id="1" onChange={e => setName(e.target.value)} class="form-control form-control-solid form-control-lg col-md-2" placeholder="نام ونام خانوادگی:"/>
+        ) : (
+          <div>
+            <div className="contain">
+              {comments.map((c, index) => (
+                <div key={index} class="border-bottom border-top p-3">
+                  <div className="mt-2 text-dark-50 font-weight-bold font-size-lg text-left max-w-1500px fontv">
+                    <div className="text-muted">{c.name}:</div>
+                    <div className="h4">{c.content}</div>
+                  </div>
                 </div>
-              </div>
-              <div class="d-flex justify-content-start ">
-                <div class=" col-md-10 rounded p-3 d-flex align-items-center">
-                  <input           
-                  onKeyDown={(e) => { 
-                      if (e.key === "Enter")
-                        sendToServer();}}
-                      id="2" onChange={e => setComment(e.target.value)} class="form-control form-control-solid form-control-lg col-md-2" placeholder="نظر شما:"/>
+              ))}
+            </div>
+            <div className="pt-4">
+              <div className="pt-2 border-top border-dark">
+                <div className="">
+                  <div className="d-flex justify-content-start">
+                    <div className="col-md-10 p-3 d-flex align-items-center">
+                      <input
+                        id="name"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        className="form-control form-control-solid form-control-lg col-md-2"
+                        placeholder="نام و نام خانوادگی:"
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-start">
+                    <div className="col-md-10 rounded p-3 d-flex align-items-center">
+                      <input
+                        id="comment"
+                        value={comment}
+                        onKeyDown={e => { if (e.key === "Enter") sendToServer(); }}
+                        onChange={e => setComment(e.target.value)}
+                        className="form-control form-control-solid form-control-lg col-md-2"
+                        placeholder="نظر شما:"
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-end p-2">
+                    <button
+                      onClick={sendToServer}
+                      className="col-md-4 btn btn-lg btn-primary font-weight-bold">
+                      ثبت
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
