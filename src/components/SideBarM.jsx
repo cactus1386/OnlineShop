eb897c137080e926c4b69859e0934eec8cd536cb
Elay <li0elay0li@gmail.com>
2024-08-04 21:23:59 -0700

    sidebar update

--- src/components/SideBarM.jsx
+++ src/components/SideBarM.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Offcanvas, Nav, Accordion } from 'react-bootstrap';
+import { Offcanvas, Nav, Accordion, ListGroup, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/SideBar.css';
+import { MenuItem } from 'react-pro-sidebar';
 
 const Sidebar = () => {
   const [show, setShow] = useState(false);
@@ -18,6 +19,7 @@ const Sidebar = () => {
     setHoveredItem(null);
   };
 
+  
   return (
     <div dir='rtl' className="sidebar-container">
       <Nav.Link variant="success" onClick={handleShow}>
@@ -33,42 +35,60 @@ const Sidebar = () => {
         <Offcanvas.Body>
           <div className="sidebar d-flex flex-column p-3 bg-light">
             <h2 className="mb-4 fontv text" dir='rtl'>لیست محصولات</h2>
-            <Accordion className='fontv'>
-              {['زنانه', 'مردانه', 'بچگانه', 'اکسسوری'].map((category, index) => (
-                <Accordion.Item eventKey={index.toString()} key={index} className="accordion-item">
-                  <Accordion.Header
-                    onMouseEnter={() => handleItemHover(category)}
-                    onMouseLeave={() => handleItemLeave()}
-                  >
-                    <i className={`bi bi-gender-${category === 'زنانه' ? 'female' : category === 'مردانه' ? 'male' : 'people'} me-2`}></i>
-                    {category}
-                  </Accordion.Header>
-                  <div 
-                    className={`mega-menu-content ${hoveredItem === category ? 'visible' : ''}`} 
-                    onMouseEnter={() => handleItemHover(category)}
-                    onMouseLeave={() => handleItemLeave()}
-                  >
-                    <div className="row">
-                      <div className="col-md-12">
-                        <h6 className="dropdown-header">Category {index + 1}</h6>
-                        <div className="content">
-                          {['Item 1', 'Item 2', 'Item 3', 'Item 4'].map(item => (
-                            <a
-                              key={item}
-                              className="dropdown-item"
-                              href="#"
-                              onClick={() => alert(`شما ${item} را انتخاب کردید!`)}
-                            >
-                              {item}
-                            </a>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </Accordion.Item>
-              ))}
-            </Accordion>
+            <Accordion >
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <i className="bi bi-gender-female me-2"></i> زنانه
+          </Accordion.Header>
+          <Accordion.Body>
+            <ListGroup variant="flush">
+            <Button>jhgb</Button>
+              <ListGroup.Item>شلوارک</ListGroup.Item>
+              <ListGroup.Item>پیراهن</ListGroup.Item>
+              <ListGroup.Item>اکسسوری</ListGroup.Item>
+            </ListGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>
+            <i className="bi bi-gender-male me-2"></i> مردانه
+          </Accordion.Header>
+          <Accordion.Body>
+            <ListGroup variant="flush">
+              <ListGroup.Item>تی شرت</ListGroup.Item>
+              <ListGroup.Item>شلوار</ListGroup.Item>
+              <ListGroup.Item>ژاکت</ListGroup.Item>
+              <ListGroup.Item>کت</ListGroup.Item>
+            </ListGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            <i className="bi bi-people me-2"></i> بچگانه
+          </Accordion.Header>
+          <Accordion.Body>
+            <ListGroup variant="flush">
+              <ListGroup.Item>تاپ</ListGroup.Item>
+              <ListGroup.Item>شلوارک</ListGroup.Item>
+              <ListGroup.Item>کفش</ListGroup.Item>
+              <ListGroup.Item>پیراهن</ListGroup.Item>
+            </ListGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+            <i className="bi bi-sunglasses me-2"></i> اکسسوری
+          </Accordion.Header>
+          <Accordion.Body>
+            <ListGroup variant="flush">
+              <ListGroup.Item>کیف</ListGroup.Item>
+              <ListGroup.Item>جواهرات</ListGroup.Item>
+              <ListGroup.Item>ساعت</ListGroup.Item>
+              <ListGroup.Item>عینک</ListGroup.Item>
+            </ListGroup>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
           </div>
         </Offcanvas.Body>
       </Offcanvas>
